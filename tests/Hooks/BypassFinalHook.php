e00fcf1902c863eae228fc393e7d19db527bf3df
samuraiseoul <scottlavigne28@gmail.com>
2019-05-24 21:19:02 -0500

    added link to testing thing

--- tests/Hooks/BypassFinalHook.php
+++ tests/Hooks/BypassFinalHook.php
@@ -5,6 +5,7 @@ namespace Essence\Hooks;
 use DG\BypassFinals;
 use PHPUnit\Runner\BeforeTestHook;
 
+// https://www.tomasvotruba.cz/blog/2019/03/28/how-to-mock-final-classes-in-phpunit/
 final class BypassFinalHook implements BeforeTestHook
 {
     public function executeBeforeTest(string $test): void
